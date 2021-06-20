92bc5e035d6423f9e906de627f6a28762a7b3d50
Masahide NAKAMURA <muretti0114@cs.kobe-u.ac.jp>
2021-06-20 15:44:38 +0900

    細かい点を修正

--- src/main/java/jp/kobespiral/hello/HelloController.java
+++ src/main/java/jp/kobespiral/hello/HelloController.java
@@ -11,34 +11,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    //あいさつのリスト
+    // あいさつのリスト
     private ArrayList<String> list = new ArrayList<>();
 
     /**
      * HTTP-GET で 猫ちゃんのあいさつページを表示する
+     * 
+     * @param name  パス・パラメータに含まれるユーザ名
+     * @param model 画面のモデル
      * @return あいさつページ
      */
     @GetMapping("/{name}/hello")
     public String SayHelloRequestParam(@PathVariable String name, Model model) {
-        model.addAttribute("greeting", createGreetingMessage()); //あいさつ文セット
-        model.addAttribute("name", name);    //ユーザ名セット
-        return "hello";                      //テンプレ hello.htmlをレンダリング
+        model.addAttribute("greeting", createGreetingMessage()); // あいさつ文セット
+        model.addAttribute("name", name); // ユーザ名セット
+        return "hello"; // テンプレ hello.htmlをレンダリング
     }
 
     /**
-     * HTTP-POST であいさつ文が追加されたときのふるまい 
+     * HTTP-POST であいさつ文を追加する
+     * 
+     * @param name  パス・パラメータに含まれるユーザ名
+     * @param aisatsu  フォームから入力されたあいさつ文
+     * @param model 画面のモデル
      * @return あいさつ追加ページ
      */
     @PostMapping("/{name}/hello/add")
-    public String addHello(@PathVariable String name, @RequestParam("text") String text, Model model) {
-        list.add(text);
-        model.addAttribute("greeting", text); //追加されたあいさつをセット
-        model.addAttribute("name", name);     //ユーザ名セット
-        return "post_hello";                  //テンプレ post_hello.htmlをレンダリング
+    public String addHello(@PathVariable String name, @RequestParam("aisatsu") String aisatsu, Model model) {
+        list.add(aisatsu);
+        model.addAttribute("greeting", aisatsu); // 追加されたあいさつをセット
+        model.addAttribute("name", name); // ユーザ名セット
+        return "post_hello"; // テンプレ post_hello.htmlをレンダリング
     }
 
     /**
      * 登録済みのあいさつを連結して、文字列にして返す
+     * 
      * @return 登録されたあいさつ文字列
      */
     private String createGreetingMessage() {
